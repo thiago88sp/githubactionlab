d1b0eb0904754bef95d70804efd8b6ebbdf0f8a6
Thiago Pontes <tpontes@fortinet.com>
2024-06-26 15:49:22 -0300

    MySQL Creation

---
+++ terraform/mysql.tf
@@ -0,0 +1,11 @@
+resource "azurerm_mysql_flexible_server" "mysql" {
+  name                   = "lw-msql-002"
+  resource_group_name    = azurerm_resource_group.rsg.name
+  location               = azurerm_resource_group.rsg.location
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  backup_retention_days  = 1
+
+  sku_name               = "GP_Standard_D2ds_v4"
+
+}
