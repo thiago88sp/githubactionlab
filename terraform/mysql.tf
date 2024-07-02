77edb790ec77de81223b26a1572461407b2f1545
Thiago Pontes <tpontes@fortinet.com>
2024-07-02 15:22:51 -0300

    Resources creation

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -1,17 +1,17 @@
-#resource "azurerm_mysql_flexible_server" "mysql" {
-#  name                   = "msql-lw-010"
-#  resource_group_name    = azurerm_resource_group.rsg.name
-#  location               = azurerm_resource_group.rsg.location
-#  administrator_login    = "psqladmin"
-#  administrator_password = "H@Sh1CoR3!"
-#  backup_retention_days  = 1
-#
-#  sku_name               = "GP_Standard_D2ds_v4"
-#
-#  zone = "1"
-#
-#  tags = {
-#    Username = "tpontes"
-#  }
-#
-#}
+resource "azurerm_mysql_flexible_server" "mysql" {
+  name                   = "msql-lw-010"
+  resource_group_name    = azurerm_resource_group.rsg.name
+  location               = azurerm_resource_group.rsg.location
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  backup_retention_days  = 1
+
+  sku_name               = "GP_Standard_D2ds_v4"
+
+  zone = "1"
+
+  tags = {
+    Username = "tpontes"
+  }
+
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
-#output "app_service_name" {
-#  value = azurerm_windows_web_app.web_app.name
-#}
+output "app_service_name" {
+  value = azurerm_windows_web_app.web_app.name
+}
 
 output "resource_group_name" {
   value = azurerm_resource_group.rsg.name
