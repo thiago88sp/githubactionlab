9c5d7666ca3eb5cc672b20c443dc3757d18c1f1d
Thiago Pontes <tpontes@fortinet.com>
2024-07-02 15:08:35 -0300

    Remove resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,31 +21,31 @@ resource "azurerm_resource_group" "rsg" {
   }
 }
 
-resource "azurerm_service_plan" "app_plan" {
-  name                = "tsp_lw_app_plan-010"
-  resource_group_name = azurerm_resource_group.rsg.name
-  location            = azurerm_resource_group.rsg.location
-  sku_name            = "B1"
-  os_type             = "Windows"
-
-  tags = {
-    Username = "tpontes"
-    Source   = "Terraform"
-  }
-}
-
-resource "azurerm_windows_web_app" "web_app" {
-  name                = "tsp-lw-web-app-0010"
-  resource_group_name = azurerm_resource_group.rsg.name
-  location            = azurerm_service_plan.app_plan.location
-  service_plan_id     = azurerm_service_plan.app_plan.id
-
-  site_config {}
-
-  https_only = true
-
-  tags = {
-    Username = "tpontes"
-    Source   = "Terraform"
-  }
-}
+#resource "azurerm_service_plan" "app_plan" {
+#  name                = "tsp_lw_app_plan-010"
+#  resource_group_name = azurerm_resource_group.rsg.name
+#  location            = azurerm_resource_group.rsg.location
+#  sku_name            = "B1"
+#  os_type             = "Windows"
+#
+#  tags = {
+#    Username = "tpontes"
+#    Source   = "Terraform"
+#  }
+#}
+#
+#resource "azurerm_windows_web_app" "web_app" {
+#  name                = "tsp-lw-web-app-0010"
+#  resource_group_name = azurerm_resource_group.rsg.name
+#  location            = azurerm_service_plan.app_plan.location
+#  service_plan_id     = azurerm_service_plan.app_plan.id
+#
+#  site_config {}
+#
+#  https_only = true
+#
+#  tags = {
+#    Username = "tpontes"
+#    Source   = "Terraform"
+#  }
+#}
